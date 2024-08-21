8b28625641e7fbb1c9ab2d1859277dcb33e2fe24
Vadym Tiron <tironvadim1@gmail.com>
2024-08-21 22:30:44 +0300

    initialize empty app

---
+++ app/models.py
@@ -0,0 +1,76 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Date,
+    Float,
+    ForeignKey,
+    func
+)
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    login = Column(String(50), unique=True, nullable=False)
+    registration_date = Column(Date, nullable=False, default=func.now())
+
+    credits = relationship("Credit", back_populates="user")
+
+
+class Credit(Base):
+    __tablename__ = "credits"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    issuance_date = Column(Date, nullable=False)
+    return_date = Column(Date, nullable=False)
+    actual_return_date = Column(Date, nullable=True)
+    body = Column(Float, nullable=False)
+    percent = Column(Float, nullable=False)
+
+    user = relationship("User", back_populates="credits")
+    payments = relationship("Payment", back_populates="credit")
+
+    @property
+    def is_closed(self):
+        return self.actual_return_date is not None
+
+    @property
+    def total_payments(self):
+        return sum(payment.sum for payment in self.payments)
+
+
+class Dictionary(Base):
+    __tablename__ = "dictionary"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(length=50), unique=True, nullable=False)
+
+
+class Plan(Base):
+    __tablename__ = "plans"
+
+    id = Column(Integer, primary_key=True, index=True)
+    period = Column(Date, nullable=False)
+    sum = Column(Float, nullable=False)
+    category_id = Column(Integer, ForeignKey("dictionary.id"), nullable=False)
+
+    category = relationship("Dictionary", back_populates="plans")
+
+
+class Payment(Base):
+    __tablename__ = "payments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    sum = Column(Float, nullable=False)
+    payment_date = Column(Date, nullable=False)
+    credit_id = Column(Integer, ForeignKey("credits.id"), nullable=False)
+    type_id = Column(Integer, ForeignKey("dictionary.id"), nullable=False)
+
+    credit = relationship("Credit", back_populates="payments")
+    payment_type = relationship("Dictionary", back_populates="payments_tp")
